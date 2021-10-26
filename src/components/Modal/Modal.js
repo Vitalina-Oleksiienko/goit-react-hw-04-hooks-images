8291cda39395fca01d5aff5ddaff35481134bc69
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-10-26 12:09:14 +0300

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,33 +1,32 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import PropTypes from 'prop-types';
 
-export default class Modal extends Component {
-    componentDidMount() {
-        document.addEventListener('keyup', e => {
-            if (e.keyCode === 27) {
-                this.props.onModalCloseByEsc();
-            }
-        });
-    }
+export default function Modal({
+  onModalCloseByEsc,
+  onModalClose,
+  onKeyDown,
+  largeImg,
+}) {
+  useEffect(() => {
+    console.log('addEventListener');
+    document.addEventListener('keyup', e => {
+      if (e.keyCode === 27) {
+        onModalCloseByEsc();
+      }
+    });
+  });
 
-    render() {
-        return (
-            <div
-                onClick={this.props.onModalClose}
-                onKeyDown={this.props.onKeyDown}
-                className="Overlay"
-            >
-                <div className="Modal">
-                    <img className="LargeGalleryImage" src={this.props.largeImg} alt="" />
-                </div>
-            </div>
-        );
-    }
+  return (
+    <div onClick={onModalClose} onKeyDown={onKeyDown} className="Overlay">
+      <div className="Modal">
+        <img className="LargeGalleryImage" src={largeImg} alt="" />
+      </div>
+    </div>
+  );
 }
 
 Modal.propTypes = {
-    onModalClose: PropTypes.func,
-    onModalCloseByEsc: PropTypes.func,
-    onModalOpen: PropTypes.func,
-    largeImg: PropTypes.string,
+  onModalClose: PropTypes.func,
+  onModalCloseByEsc: PropTypes.func,
+  largeImg: PropTypes.string,
 };
